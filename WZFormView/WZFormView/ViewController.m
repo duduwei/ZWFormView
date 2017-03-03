80c463382a12bd9e6d3e07dd40289c51efd1ddea
weizhenwei <850076479@qq.com>
2017-03-03 18:25:16 +0800

    搭建小框架
    导航栏,pch文件，搭建页面

--- WZFormView/WZFormView/ViewController.m
+++ WZFormView/WZFormView/ViewController.m
@@ -16,14 +16,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"WZFormView";
+    
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)viewWillAppear:(BOOL)animated
+{
+    NSArray* titleArr = [NSArray arrayWithObjects:@"类型1：数据完整",@"类型2：多种情况",@"类型2：多种情况", nil];
+    
+    for(int i=0; i<titleArr.count; i++)
+    {
+        UIButton* bton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [bton setTitle:titleArr[i] forState:UIControlStateNormal];
+        [bton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        bton.layer.cornerRadius = 5;
+        bton.layer.masksToBounds = YES;
+        bton.layer.borderColor = [UIColor grayColor].CGColor;
+        bton.layer.borderWidth = 0.5;
+        bton.tag = 800+i;
+        //[bton setBackgroundColor:[UIColor grayColor]];
+        bton.frame = CGRectMake(15, 100+50*i, SCREEN_WIDTH-30, 44);
+        
+        [bton addTarget:self action:@selector(gotoShowVC:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [self.view addSubview:bton];
+    }
 }
+
+-(void)gotoShowVC:(UIButton*)bton
+{
+    if(bton.tag == 801)
+    {
+        
+    }
+    else if (bton.tag == 802)
+    {
+        
+    }
+    else if (bton.tag == 802)
+    {
+        
+    }
+    
+}
+
 
 
 @end
---
+++ WZFormView/WZFormView/define.h
@@ -0,0 +1,18 @@
+//
+//  define.h
+//  WZFormView
+//
+//  Created by wei on 2017/3/3.
+//  Copyright © 2017年 weizhenwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//获取屏幕宽度、高度
+#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+
+
+@interface define : NSObject
+
+@end
